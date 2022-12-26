fd59dd1dc80a147aa4ffec17fea687bcd301d2d6
CanBeBlamed <hunfoil@protonmail.com>
2022-12-26 02:58:28 +0100

    remove

--- lua/zdarvai/packer.lua
+++ lua/zdarvai/packer.lua
@@ -13,16 +13,8 @@ return require('packer').startup(function(use)
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
-  use({
-    'rose-pine/neovim',
-    as = 'rose-pine',
-    config = function()
-        vim.cmd('colorscheme rose-pine')
-    end
-})
 
   use('nvim-treesitter/playground')
-  use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
   use('Shougo/deoplete.nvim')
